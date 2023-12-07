9e13a00a9a8ab2db41506750ccceab42fe81bf08
ArtunKARA <artunankara@gmail.com>
2023-12-07 04:58:26 +0300

    add event page

---
+++ assets/js/cart.js
@@ -0,0 +1,77 @@
+
+var miktarInputs = document.querySelectorAll('.checkout-table input[type="number"]');
+
+var sepetiGuncelleButton = document.getElementById('update-btn');
+
+var kaldirmaButonlari = document.querySelectorAll('.pr-remove a');
+
+var sepetiOnaylaButton = document.getElementById('aprove-btn');
+
+miktarInputs.forEach(function(input) {
+  input.addEventListener('change', function() {
+    sepetiGuncelle(); 
+  });
+});
+
+
+kaldirmaButonlari.forEach(function(button) {
+  button.addEventListener('click', function(event) {
+    event.preventDefault();
+    urunuKaldir(button);
+    sepetiGuncelle(); 
+  });
+});
+
+
+sepetiGuncelleButton.addEventListener('click', function() {
+  sepetiGuncelle(); 
+});
+
+
+sepetiOnaylaButton.addEventListener('click', function() {
+  sepetiOnayla();
+});
+
+// Ürünü sepetten kaldırma fonksiyonu
+function urunuKaldir(button) {
+  var urunSatiri = button.closest('tr'); 
+  urunSatiri.remove(); 
+}
+
+// Sepeti güncelleme fonksiyonu
+function sepetiGuncelle() {
+    var totalPrice = 0;
+
+    // Her ürün satırını döngüye al ve toplam tutarı hesapla
+    var productRows = document.querySelectorAll('.checkout-table tbody tr');
+
+    productRows.forEach(function(row) {
+      var quantityInput = row.querySelector('input[type="number"]');
+      var quantity = parseInt(quantityInput.value || 0); // Güvenlik için varsayılan değer
+      var priceString = row.querySelector('.product-title.font-alt').textContent.trim();
+      var price = parseFloat(priceString.split(' ')[0].replace(',', '.')); // Fiyatı alma
+
+      var rowTotal = quantity * price;
+      totalPrice += rowTotal;
+
+      // Yeni toplamı ekrana yaz
+      var totalElement = row.querySelector('.product-title.font-alt');
+      if (totalElement) {
+        totalElement.textContent = rowTotal.toFixed(2).replace('.', ',') + ' TL';
+      }
+    });
+
+    // Toplam tutarı ve sepet toplamlarını güncelle
+    var cartTotalRows = document.querySelectorAll('.shop-Cart-totalbox tbody tr');
+
+    cartTotalRows.forEach(function(row, index) {
+      if (index === 2) {
+        row.querySelector('td').textContent = totalPrice.toFixed(2).replace('.', ',') + ' TL'; // Toplam tutarı güncelle
+      }
+    });
+}
+
+// Sepeti onayla fonksiyonu
+function sepetiOnayla() {
+  alert('Sepetiniz onaylandı!');
+}
